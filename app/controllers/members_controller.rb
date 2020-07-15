06c5551456356e5a5cb878c38326bd7919be638e
Sam Evans <evansst@icloud.com>
2020-07-15 16:01:09 -0600

    Adds member controller CRUD methods

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,20 +2,33 @@ class MembersController < ApplicationController
   
   def index
     @members = Member.all
+    
     render json: @members
+  end
+
+  def show
+    @member = Member.find(params[:id])
+    
+    render json: @member
+  end
+
+  def create
+    @member = Member.create(name: params[:name], email: params[:email])
+    
+    render json: @member
   end
 
   def update
     @member = Member.find(params[:id])
     @member.update(email: params[:email])
 
-    # render json: "Member #{@member.id} updated mail to #{@member.email}"
     redirect_to members_path
   end
 
   def destroy
     @member = Member.find(params[:id])
     @member.destroy
+    
     render json: "#{@member.name} was destroyed"
   end
 end
